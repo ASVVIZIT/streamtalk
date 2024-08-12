d642d7769a8ac474aa3273f2a2eabb1284486c8b
Sergey Andrianov <DillerASV@yandex.ru>
2024-08-12 06:46:13 +0500

    fix bag

--- src/StreamTalkServiceProvider.php
+++ src/StreamTalkServiceProvider.php
@@ -29,7 +29,7 @@ class StreamTalkServiceProvider extends ServiceProvider
     public function boot()
     {
         // Load Views and Routes
-        $this->loadViewsFrom(__DIR__ . '/views', 'StreamTalk');
+        $this->loadViewsFrom(__DIR__ . '/views', 'streamtalk');
         $this->loadRoutes();
 
         if ($this->app->runningInConsole()) {
@@ -49,38 +49,40 @@ class StreamTalkServiceProvider extends ServiceProvider
     protected function setPublishes()
     {
         // Load user's avatar folder from package's config
-        $userAvatarFolder = json_decode(json_encode(include(__DIR__.'/config/StreamTalk.php')))->user_avatar->folder;
+        $userAvatarFolder = json_decode(json_encode(include(__DIR__.'/config/streamtalk.php')))->user_avatar->folder;
 
         // Config
         $this->publishes([
-            __DIR__ . '/config/StreamTalk.php' => config_path('StreamTalk.php')
-        ], 'StreamTalk-config');
+            __DIR__ . '/config/streamtalk.php' => config_path('streamtalk.php')
+        ], 'streamtalk-config');
 
         // Migrations
+
+        $separator = `_`;
         $this->publishes([
-            __DIR__ . '/database/migrations/2022_01_10_99999_add_active_status_to_users.php' => database_path('migrations/' . date('Y_m_d') . '_999999_add_active_status_to_users.php'),
-            __DIR__ . '/database/migrations/2022_01_10_99999_add_avatar_to_users.php' => database_path('migrations/' . date('Y_m_d') . '_999999_add_avatar_to_users.php'),
-            __DIR__ . '/database/migrations/2022_01_10_99999_add_dark_mode_to_users.php' => database_path('migrations/' . date('Y_m_d') . '_999999_add_dark_mode_to_users.php'),
-            __DIR__ . '/database/migrations/2022_01_10_99999_add_messenger_color_to_users.php' => database_path('migrations/' . date('Y_m_d') . '_999999_add_messenger_color_to_users.php'),
-            __DIR__ . '/database/migrations/2022_01_10_99999_create_StreamTalk_favorites_table.php' => database_path('migrations/' . date('Y_m_d') . '_999999_create_StreamTalk_favorites_table.php'),
-            __DIR__ . '/database/migrations/2022_01_10_99999_create_StreamTalk_messages_table.php' => database_path('migrations/' . date('Y_m_d') . '_999999_create_StreamTalk_messages_table.php'),
-        ], 'StreamTalk-migrations');
+            __DIR__ . '/database/migrations/2022_01_10_99999_add_active_status_to_users.php' => database_path('migrations/' . date('Y_m_d') . $separator . date('H_i_s') . $separator . '999999_add_active_status_to_users.php'),
+            __DIR__ . '/database/migrations/2022_01_10_99999_add_avatar_to_users.php' => database_path('migrations/' . date('Y_m_d') . $separator . date('H_i_s') . $separator . '999999_add_avatar_to_users.php'),
+            __DIR__ . '/database/migrations/2022_01_10_99999_add_dark_mode_to_users.php' => database_path('migrations/' . date('Y_m_d') . $separator . date('H_i_s') . $separator . '999999_add_dark_mode_to_users.php'),
+            __DIR__ . '/database/migrations/2022_01_10_99999_add_messenger_color_to_users.php' => database_path('migrations/' . date('Y_m_d') . $separator . date('H_i_s') . $separator . 'add_messenger_color_to_users.php'),
+            __DIR__ . '/database/migrations/2022_01_10_99999_create_streamtalk_favorites_table.php' => database_path('migrations/' . date('Y_m_d') . $separator . date('H_i_s') . $separator . 'create_streamtalk_favorites_table.php'),
+            __DIR__ . '/database/migrations/2022_01_10_99999_create_streamtalk_messages_table.php' => database_path('migrations/' . date('Y_m_d') . $separator . date('H_i_s') . $separator . 'create_streamtalk_messages_table.php'),
+        ], 'streamtalk-migrations');
 
         // Models
         $isV8 = explode('.', app()->version())[0] >= 8;
         $this->publishes([
             __DIR__ . '/Models' => app_path($isV8 ? 'Models' : '')
-        ], 'StreamTalk-models');
+        ], 'streamtalk-models');
 
         // Controllers
         $this->publishes([
             __DIR__ . '/Http/Controllers' => app_path('Http/Controllers/vendor/StreamTalk')
-        ], 'StreamTalk-controllers');
+        ], 'streamtalk-controllers');
 
         // Views
         $this->publishes([
             __DIR__ . '/views' => resource_path('views/vendor/StreamTalk')
-        ], 'StreamTalk-views');
+        ], 'streamtalk-views');
 
         // Assets
         $this->publishes([
@@ -91,13 +93,13 @@ class StreamTalkServiceProvider extends ServiceProvider
             // Images
             __DIR__ . '/assets/imgs' => storage_path('app/public/' . $userAvatarFolder),
              // CSS
-             __DIR__ . '/assets/sounds' => public_path('sounds/StreamTalk'),
-        ], 'StreamTalk-assets');
+             __DIR__ . '/assets/sounds' => public_path('sounds/streamtalk'),
+        ], 'streamtalk-assets');
 
         // Routes (API and Web)
         $this->publishes([
-            __DIR__ . '/routes' => base_path('routes/StreamTalk')
-        ], 'StreamTalk-routes');
+            __DIR__ . '/routes' => base_path('routes/streamtalk')
+        ], 'streamtalk-routes');
     }
 
     /**
@@ -109,10 +111,10 @@ class StreamTalkServiceProvider extends ServiceProvider
     {
         if (config('streamtalk.routes.custom')) {
             Route::group($this->routesConfigurations(), function () {
-                $this->loadRoutesFrom(base_path('routes/StreamTalk/web.php'));
+                $this->loadRoutesFrom(base_path('routes/streamtalk/web.php'));
             });
             Route::group($this->apiRoutesConfigurations(), function () {
-                $this->loadRoutesFrom(base_path('routes/StreamTalk/api.php'));
+                $this->loadRoutesFrom(base_path('routes/streamtalk/api.php'));
             });
         } else {
             Route::group($this->routesConfigurations(), function () {
